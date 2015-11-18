5b5c3fc02978397b3c4eae6cd4fce545e77d040b
GameMoon <ugrindaniel@gmail.com>
2015-11-18 17:17:45 +0100

    valami

--- server/Main.java
+++ server/Main.java
@@ -5,11 +5,8 @@ public class Main {
 
     public static void main(String[] args){
         System.out.println("Server started " + Constants.versionNumber);
-
         GameManager gameManager = new GameManager();
         CommandListener commandListener = new CommandListener(gameManager);
-
-
     }
 
 }
